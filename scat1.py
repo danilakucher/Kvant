0b263b9f280e65da47db24c3a527a0682f2def36
Maxim5 <sosed512@gmail.com>
2024-11-09 10:55:30 +0800

    "Добавлен код для визуализации волновой функции"

--- scat1.py
+++ scat1.py
@@ -54,8 +54,33 @@ def ampD2(E,a=1,u0=20):
   k = np.emath.sqrt(E)
   kp = np.emath.sqrt(E+u0)
   return 2*np.exp(1j*k*a)/(2*np.cos(kp*a)+1j*(k/kp+kp/k)*np.sin(kp*a))
+ 
+def ampC1(E, a, u0):
+  """
+  
+  """
+  k = np.emath.sqrt(E)
+  kp = np.emath.sqrt(E+u0)
+  
+  return 0.5*ampD(E, a, u0)*np.exp(1j*(k-kp)*a)*(1+k/kp)
 
+def ampC2(E, a, u0):
+  """
+  
+  """
+  k = np.emath.sqrt(E)
+  kp = np.emath.sqrt(E+u0)
+  
+  return 0.5*ampD(E, a, u0)*np.exp(1j*(k+kp)*a)*(1-k/kp)
 
+def ampB(E, a, u0):
+  """
+  
+  """
+  k = np.emath.sqrt(E)
+  kp = np.emath.sqrt(E+u0)
+  
+  return (1j*0.5)*ampD(E, a, u0)*np.exp(1j*k*a)*(kp/k-k/kp)*np.sin(kp*a)
 
 def spectre(E,a=1,u0=20):
   kap = np.emath.sqrt(-E)
@@ -80,10 +105,26 @@ def plotAmp(l=-1, r=1, b=-1,t=1,Nx=100,Ny=100,a=1,u0=20):
   ax.set_ylabel(r'$\mathrm{Im}(E)$')
   ax.legend()
    
+def psi(x, E, a=1, u0=20):
+  """
+  Calculate wave function of scattery beam.
+  """
 
-def psi(x):
-  """
-  Plot wave function.
-  """
-  pass
+  D = ampD(E, a, u0)
+  C1 = ampC1(E, a, u0)
+  C2 = ampC2(E, a, u0)
+  B = ampB(E, a, u0)
+  
+  ind1 = x<0
+  ind2 = (x>0) & (x<=a)
+  ind3 = x>a
+  k = np.emath.sqrt(E)
+  kp = np.emath.sqrt(E+u0)
+  x1 = x[ind1]
+  x2 = x[ind2]
+  x3 = x[ind3]
+  z1 = np.exp(1j*k*x1)+B*np.exp(-1j*k*x1)
+  z2 = C1*np.exp(1j*kp*x2)+C2*np.exp(-1j*kp*x2)
+  z3 = D*np.exp(1j*k*x3)
+  return np.r_[z1, z2, z3]
   
